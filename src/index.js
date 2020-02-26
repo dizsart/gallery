cd7caf0124dc984ff1c7397fe590e47f98676480
dizsart <darogadibia@gmail.com>
2020-02-26 14:24:31 +0100

    added some changes to the index.js file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const tasks = ['take out the trash', 'shovel the driveway', 'walk the dog'];
+function Title(){
+    return <h1> Task List</h1>
+}
+function tick() {
+const element = (
+    
+<div align="center">
+    <Title/>
+    <h1>Hello World!</h1>
+<h2>It is {new Date().toLocaleTimeString()}.</h2>
+    <ol>
+    {tasks.map((task, index) => 
+    <li key = {index}> {task} </li>)}
+    </ol>
+
+</div>
+);
+
+ReactDOM.render(element, document.getElementById('root'))
+}
+
+setInterval(tick, 1000);
